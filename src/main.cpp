d4f23ea42f3663253c3436e866d7bcc11be32bd6
Anton Nurapkin <nurapkin99@mail.ru>
2025-04-09 12:21:16 +0300

    feat: add exceptions catching

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
-﻿#include "solver/solver.h"
+﻿#include <iostream>
+#include "solver/solver.h"
 #include "preprocessor/preprocessor.h"
 #include "postprocessor/postprocessor.h"
+#include "preprocessor/error.h"
 
 
 int main(int argc, char* argv[])
 {
-	Preprocessor preprocessor(argc, argv);
+	try {
+		Preprocessor preprocessor(argc, argv);
+		preprocessor.readConfig();
 
-	preprocessor.readConfig();
+		Solver solver(preprocessor);
+		solver.run();
 
-	Solver solver(preprocessor);
-
-	solver.run();
-
-	Postprocessor postprocessor(preprocessor, solver);
-
-	postprocessor.run();
+		Postprocessor postprocessor(preprocessor, solver);
+		postprocessor.run();
+	} catch (const PreprocessorError& err) {
+		std::cout << "PreprocessorError:\n" <<err.what();
+	} catch (const std::exception& err) {
+		std::cout << err.what();
+	} catch (...) {
+		std::cerr << "Caught an unknown exception!\n";
+	}
+	std::cout << "The program has finished working\n";
 
     return 0;
 }
